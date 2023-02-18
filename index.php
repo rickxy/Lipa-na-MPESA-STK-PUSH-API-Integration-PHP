49702a5bb2f2b3520ca478e8455153324a6e27fa
NetGo+ Dev <36708000+rickxy@users.noreply.github.com>
2023-02-18 23:49:01 +0300

    Update index.php

--- index.php
+++ index.php
@@ -50,7 +50,6 @@
                         </div>
                     </form>
                 </div>
-
             </div>
 
             <div class="powered">App powered by <a href="#" target="_blank">NetGO+</a></div>
