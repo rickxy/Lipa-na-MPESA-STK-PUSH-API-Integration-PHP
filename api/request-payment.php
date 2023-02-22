c97418809e405f839a45df9f2e7fddc6ff78f4b0
NetGo+ Dev <36708000+rickxy@users.noreply.github.com>
2023-02-22 23:56:05 +0300

    Update request-payment.php

--- api/request-payment.php
+++ api/request-payment.php
@@ -2,6 +2,7 @@
 require('config.php');
 
 session_start();
+
 $base     = (isset($_SERVER["HTTPS"]) ? "https" : "http") . "://" . (isset($_SERVER["SERVER_NAME"]) ? $_SERVER["SERVER_NAME"] : '');
 $defaults = array(
     "env"              => "sandbox",
